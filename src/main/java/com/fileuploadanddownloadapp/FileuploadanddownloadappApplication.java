6cfb9b89369f0612408b7d97a9a6ffae0587f5db
aleynayesil <aleyna.yesil@freightest.com>
2022-12-15 14:24:51 +0300

    Add docker file

---
+++ src/main/java/com/fileuploadanddownloadapp/FileuploadanddownloadappApplication.java
@@ -0,0 +1,13 @@
+package com.fileuploadanddownloadapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadanddownloadappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadanddownloadappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fileuploadanddownloadapp/FileuploadanddownloadappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileuploadanddownloadapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadanddownloadappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
